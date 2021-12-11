a7834926c45a1211001b16f6cda96b94a88e3f00
sergio rodriguez <sarv9208@hotmail.com>
2021-12-11 16:05:04 -0500

    Implementacion de Front en Heroku 2

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/aerolineade-bajo-costo'));
+app.use(express.static('./dist/AerolineadeBajoCosto'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/aerolineade-bajo-costo/'}),
+    res.sendFile('index.html', {root: 'dist/AerolineadeBajoCosto/'}),
 );
 
 // Start the app by listening on the default Heroku port
